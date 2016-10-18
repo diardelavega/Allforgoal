96e2003f516e310c19ff25d5dddeb02d416ff126
diardelavega <diardelavega@gmail.com>
2016-10-18 12:46:58 +0300

    nothing

--- WebContent/NewTest/displays.js
+++ WebContent/NewTest/displays.js
@@ -2,23 +2,29 @@
  * create the box, on which the data from the mpl in the all matches page is
  * going to be displayed
  */
-function mplBoxCreate(res) {
-var tab= document.createElement("table");
-var tdTxt;
-for(var i=0;i<allMAtchPredLine.length;i++)	{
-
-	tdTxt="<tr data-toggle='collapse' data-target='#collapsePanel"+i+"' id='rowmld"+i+"' onclick= 'clickCatch("+i+");' >"+
-				"<td><span class='glyphicon glyphicon-triangle-bottom' data-toggle='collapse' data-target='#collapsePanel"+i+"'> </td>"+
-				"<td>home team	</td>  <td>FT1</td> <td>HT1</td> <td>Ht2</td>  <td>FT2</td>"+
-				"<td>home win	</td>  <td>draw</td> <td>away win	</td> <td>over</td>  <td>under</td>"+
-				"<td></td>  <td></td> "+
-				"<td>HT: 1 > tot. Score	</td>  <td>HT: 2 < tot. Score	</td> "+
-				"<td>HT: 2 > tot. Score	</td>  <td>tot. HT Score	</td> "+
-				"<td>tot. FT Score</td>"+
-			"</tr>"
-
-
-	$("#tab1").children('tbody').append(tdTxt);
-	$("#tab1").children('tbody').append(collapsablePannelCreator(i));
-}
+function mplBoxCreate() {
+	var tab= document.createElement("table");
+	$(tab).css("border",1);
+	var thed= document.createElement("thead");
+	var tbod= document.createElement("tbody");
+	$(tab).append(thed);
+	$(tab).append(tbod);
+	var tdTxt;
+	//for(var i=0;i<allMAtchPredLine.length;i++)	{
+	for(var i=0;i<3;i++)	{
+		tdTxt="<tr data-toggle='collapse' data-target='#collapsePanel"+i+"' id='rowmld"+i+"' onclick= 'clickCatch("+i+");' >"+
+					"<td><span class='glyphicon glyphicon-triangle-bottom' data-toggle='collapse' data-target='#collapsePanel"+i+"'> </td>"+
+					"<td>home team	</td>  <td>FT1</td> <td>HT1</td> <td>Ht2</td>  <td>FT2</td>"+
+					"<td>home win	</td>  <td>draw</td> <td>away win	</td> <td>over</td>  <td>under</td>"+
+					"<td></td>  <td>"+i+"</td> "+
+					"<td>HT: 1 > tot. Score	</td>  <td>HT: 2 < tot. Score	</td> "+
+					"<td>HT: 2 > tot. Score	</td>  <td>tot. HT Score	</td> "+
+					"<td>tot. FT Score</td>"+
+				"</tr>"
+	
+	$(tbod).append(tdTxt);
+	//	$("#tab1").children('tbody').append(tdTxt);
+	//	$("#tab1").children('tbody').append(collapsablePannelCreator(i));
+	}
+	$("#div1").append(tab);
 }
